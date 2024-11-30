4ad24cccb54f4f5a3e2ece7fc2fbef322f35954a
nguyenthanhlinh22 <thanhlinhnguyen22032004@gmail.com>
2024-11-30 23:19:23 +0700

    done

--- src/page/Setting/ContactForm.jsx
+++ src/page/Setting/ContactForm.jsx
@@ -20,15 +20,12 @@ const ContactForm = () => {
     });
   };
 
-  // Hàm validate form
   const validateForm = () => {
     const newErrors = {};
 
     if (!formData.topic) newErrors.topic = "Vui lòng chọn vấn đề cần liên hệ.";
     if (!formData.email) newErrors.email = "Email không được để trống.";
-    else if (
-      !/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData.email) // Kiểm tra định dạng email
-    )
+    else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData.email))
       newErrors.email = "Email không đúng định dạng.";
     if (!formData.message)
       newErrors.message = "Vui lòng nhập nội dung cần hỗ trợ.";
@@ -39,14 +36,13 @@ const ContactForm = () => {
       newErrors.dataProcessing = "Bạn cần đồng ý với chính sách xử lý dữ liệu.";
 
     setErrors(newErrors);
-    return Object.keys(newErrors).length === 0; // Trả về true nếu không có lỗi
+    return Object.keys(newErrors).length === 0;
   };
 
-  // Hàm xử lý gửi form
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!validateForm()) return; // Nếu lỗi thì không tiếp tục
+    if (!validateForm()) return;
 
     try {
       const response = await fetch("/Contact", {
@@ -67,146 +63,47 @@ const ContactForm = () => {
           acknowledge: false,
           dataProcessing: false,
         });
+        setErrors({});
+      } else {
+        console.error("Có lỗi xảy ra khi gửi dữ liệu.");
+      }
+    } catch (error) {
+      console.error("Lỗi kết nối:", error);
+    }
+  };
 
-<<<<<<< HEAD
-=======
-    };
+  return (
+    <div className="bg-gray-900 min-h-screen flex items-center justify-center pt-3">
+      <div>
+        <h1 className="text-3xl font-semibold text-white mb-6 mt-16">
+          Liên hệ với chúng tôi
+        </h1>
+        <div className="py-14 px-[190px] rounded-lg max-w-4xl w-full border-4 border-indigo-500/5">
+          <p className="text-gray-400 mb-6">
+            Chúng tôi luôn ghi nhận các đóng góp ý kiến của bạn để cải tiến và
+            nâng cấp sản phẩm SoundWave ngày một hoàn thiện và hữu ích hơn.
+          </p>
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        console.log('Form submitted', formData);
-    };
-
->>>>>>> 90f2bc4959555056c931cbb776191c14f1bd4607
-    return (
-        <div className='bg-gray-900 min-h-screen flex items-center justify-center pt-3 '>
-            <div>
-                <h1 className='text-3xl font-semibold text-white mb-6 mt-16 '>
-                    Liên hệ với chúng tôi
-                </h1>
-                <div className='  py-14 px-[190px] rounded-lg max-w-4xl w-full border-4 border-indigo-500/5'>
-                    <p className='text-gray-400  mb-6'>
-                        Chúng tôi luôn ghi nhận các đóng góp ý kiến của bạn để
-                        cải tiến và nâng cấp sản phẩm SoundWave ngày một hoàn
-                        thiện và hữu ích hơn. Đừng ngại chia sẻ ý tưởng cho
-                        chúng tôi.
-                    </p>
-
-                    <form onSubmit={handleSubmit}>
-                        <div className='mb-4'>
-                            <label className='block mb-2 text-sm font-medium text-gray-400'>
-                                Chọn vấn đề mà bạn cần hỗ trợ{' '}
-                                <span className='text-red-600'> *</span>
-                            </label>
-                            <select
-                                name='topic'
-                                value={formData.topic}
-                                onChange={handleInputChange}
-                                className='w-full p-3 bg-gray-900 text-gray-200 rounded border border-pink-500'
-                                required
-                            >
-                                <option value=''>
-                                    Chọn vấn đề cần liên hệ
-                                </option>
-                                <option value='technical'>
-                                    Hỗ trợ kỹ thuật
-                                </option>
-                                <option value='billing'>Hóa đơn</option>
-                                <option value='general'>Câu hỏi chung</option>
-                            </select>
-                        </div>
-
-                        <div className='mb-5'>
-                            <label className='block mb-2 text-sm font-medium text-gray-400'>
-                                Email <span className='text-red-600'> *</span>
-                            </label>
-                            <input
-                                type='email'
-                                name='email'
-                                value={formData.email}
-                                onChange={handleInputChange}
-                                className='w-full p-3 bg-gray-900 text-gray-200 rounded border border-pink-500'
-                                required
-                            />
-                        </div>
-
-                        <div className='mb-5'>
-                            <label className='block mb-2 text-sm font-medium text-gray-400'>
-                                Tên người dùng
-                            </label>
-                            <input
-                                type='text'
-                                name='username'
-                                value={formData.username}
-                                onChange={handleInputChange}
-                                className='w-full p-3 bg-gray-900 text-gray-200 rounded border border-pink-500'
-                            />
-                            <p className='text-sm text-gray-400 mt-2'>
-                                Có thể tìm thấy tên người dùng trên hồ sơ
-                                SoundWave của tài khoản.
-                            </p>
-                        </div>
-
-                        <div className='mb-4'>
-                            <label className='block mb-2 text-sm font-medium text-gray-400'>
-                                Nội dung cần giúp đỡ{' '}
-                                <span className='text-red-600'> *</span>
-                            </label>
-                            <textarea
-                                name='message'
-                                value={formData.message}
-                                onChange={handleInputChange}
-                                className='w-full p-3 bg-gray-900 text-gray-200 rounded border border-pink-500'
-                                rows='4'
-                                required
-                            />
-                        </div>
-
-                        <div className='mb-4'>
-                            <p className='text-sm text-gray-400 mb-2'>
-                                Xác nhận rằng nội dung trình bày là thật trước
-                                khi gửi
-                            </p>
-                            <div className='flex items-center mb-2'>
-                                <input
-                                    type='checkbox'
-                                    name='acknowledge'
-                                    checked={formData.acknowledge}
-                                    onChange={handleInputChange}
-                                    className='mr-2 border border-pink-500'
-                                    required
-                                />
-                                <label className='text-gray-400 text-sm'>
-                                    Tôi đảm bảo rằng tất cả thông tin là đúng và
-                                    chính xác.
-                                </label>
-                            </div>
-                            <div className='flex items-center'>
-                                <input
-                                    type='checkbox'
-                                    name='dataProcessing'
-                                    checked={formData.dataProcessing}
-                                    onChange={handleInputChange}
-                                    className='mr-2 border border-pink-500'
-                                    required
-                                />
-                                <label className='text-gray-400 text-sm'>
-                                    Tôi đồng ý với chính sách xử lý dữ liệu của
-                                    SoundWave.
-                                </label>
-                            </div>
-                        </div>
-
-<<<<<<< HEAD
-=======
-                        <button
-                            type='submit'
-                            className='w-full bg-gradient-to-r from-[#FF553E] to-[#FF0065] hover:bg-pink-500 text-white py-3 rounded-lg font-semibold text-center'
-                        >
-                            Xác Nhận
-                        </button>
-                    </form>
-                </div>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label className="block mb-2 text-sm font-medium text-gray-400">
+                Chọn vấn đề mà bạn cần hỗ trợ <span className="text-red-600"> *</span>
+              </label>
+              <select
+                name="topic"
+                value={formData.topic}
+                onChange={handleInputChange}
+                className={`w-full p-3 bg-gray-900 text-gray-200 rounded border ${
+                  errors.topic ? "border-red-500" : "border-pink-500"
+                }`}
+                required
+              >
+                <option value="">Chọn vấn đề cần liên hệ</option>
+                <option value="technical">Hỗ trợ kỹ thuật</option>
+                <option value="billing">Hóa đơn</option>
+                <option value="general">Câu hỏi chung</option>
+              </select>
+              {errors.topic && <p className="text-red-500 text-sm">{errors.topic}</p>}
             </div>
 
             <div className="mb-5">
@@ -221,10 +118,9 @@ const ContactForm = () => {
                 className={`w-full p-3 bg-gray-900 text-gray-200 rounded border ${
                   errors.email ? "border-red-500" : "border-pink-500"
                 }`}
+                required
               />
-              {errors.email && (
-                <p className="text-red-500 text-sm mt-1">{errors.email}</p>
-              )}
+              {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
             </div>
 
             <div className="mb-5">
@@ -239,8 +135,7 @@ const ContactForm = () => {
                 className="w-full p-3 bg-gray-900 text-gray-200 rounded border border-pink-500"
               />
               <p className="text-sm text-gray-400 mt-2">
-                Có thể tìm thấy tên người dùng trên hồ sơ SoundWave của tài
-                khoản.
+                Có thể tìm thấy tên người dùng trên hồ sơ SoundWave của tài khoản.
               </p>
             </div>
 
@@ -257,9 +152,7 @@ const ContactForm = () => {
                 }`}
                 rows="4"
               />
-              {errors.message && (
-                <p className="text-red-500 text-sm mt-1">{errors.message}</p>
-              )}
+              {errors.message && <p className="text-red-500 text-sm">{errors.message}</p>}
             </div>
 
             <div className="mb-4">
@@ -300,7 +193,6 @@ const ContactForm = () => {
               )}
             </div>
 
->>>>>>> 90f2bc4959555056c931cbb776191c14f1bd4607
             <button
               type="submit"
               className="w-full bg-gradient-to-r from-[#FF553E] to-[#FF0065] hover:bg-pink-500 text-white py-3 rounded-lg font-semibold text-center"
