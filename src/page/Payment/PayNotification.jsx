36f82ae3f70d0ae39771f57c3c7d368e9fe4c6ea
longtien1998 <125441317+longtien1998@users.noreply.github.com>
2024-12-12 00:51:31 +0700

    fix payment

--- src/page/Payment/PayNotification.jsx
+++ src/page/Payment/PayNotification.jsx
@@ -72,7 +72,7 @@ function PayNotification() {
                     <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
                 </div>
             ) : notifi ? (
-                <PaySuccess type={payment.type}/>
+                <PaySuccess/>
             ) : (
                 <PayFail />
             )}
