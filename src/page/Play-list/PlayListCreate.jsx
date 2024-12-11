8ea80bba251e2f18b5660b9570a981fa902f5b2b
1QuoccouQ1 <117536308+1QuoccouQ1@users.noreply.github.com>
2024-12-12 02:54:20 +0700

    Update PlayListCreate.jsx

---
+++ src/page/Play-list/PlayListCreate.jsx
@@ -0,0 +1,11 @@
+function PlayListCreate() {
+    return ( <>
+        <div className="fixed top-0 bottom-0 right-0 left-0 z-20  flex items-center justify-center text-white bg-slate-800/40 ">
+        <div className="w-[600px] h-[350px] rounded-md bg-medium z-20">
+          
+        </div>
+      </div>
+    </> );
+}
+
+export default PlayListCreate;
